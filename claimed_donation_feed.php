721d68289a696f037209e790c63a2cfd884a2d06
frostic3 <frostic3@users.noreply.github.com>
2015-04-04 21:18:34 +0530

    Clean among other things
    ->Retrieve delivered_at from db and store in donation array to be sent
    to application
    ->Add server side validation

--- claimed_donation_feed.php
+++ claimed_donation_feed.php
@@ -1,7 +1,6 @@
 <?php
 
 error_reporting(E_ALL ^ E_DEPRECATED);
-
 // array for JSON response
 $response = array();
 
@@ -12,30 +11,60 @@ if (!(empty($_POST['email']))) {
     $db = new DB_CONNECT();
 
     $email = $_POST['email'];
+    $tag = $_POST['tag'];
 
     //Get oid from the email
     $result = mysql_query("SELECT oid from orphanages WHERE email = '$email'");
     $result = mysql_fetch_array($result);
     $oid = $result["oid"];
-
-    $result = mysql_query("SELECT *FROM donations WHERE oid = $oid ORDER BY claimed_at DESC") or die(mysql_error());
-
+    if($tag == "MyClaimedDonations")
+        $result = mysql_query("SELECT *FROM donations WHERE oid = $oid AND delivered = 0 ORDER BY claimed_at DESC") or die(mysql_error());
+    else if($tag == "markAsDelivered"){
+        $did = $_POST['donationid'];
+        $currentTime = (new \DateTime())->format('Y-m-d H:i:s');
+        $result = mysql_query("UPDATE donations SET delivered = 1, delivered_at = '$currentTime' WHERE donationid = $did");
+        // check if row inserted or not
+        if ($result) {
+            // successfully updated database
+            $response["success"] = 1;
+            $response["message"] = "Donation successfully marked as received!";
+            // echoing JSON response
+            echo json_encode($response);
+        } else {
+            // failed to insert row
+            $response["success"] = 0;
+            $response["message"] = "Oops! An error occurred while claiming the donation.";
+            // echoing JSON response
+            echo json_encode($response);
+        }
+        exit;
+    }
+    else
+        $result = mysql_query("SELECT *FROM donations WHERE oid = $oid AND delivered = 1 ORDER BY delivered_at DESC") or die(mysql_error());
     // check for empty result
-    if (mysql_num_rows($result) > 0) {
+    if (mysql_num_rows($result) > 0 ) {
         // looping through all results
         // products node
-        $response["claimedDonations"] = array();
+        $response["SelectedDonationsFeed"] = array();
 
         while ($row = mysql_fetch_array($result)) {
             // temp user array
             $donation = array();
             $donation["donationid"] = $row["donationid"];
+
             $time = $row["created_at"];
             $timestamp = strtotime( $time );
             $donation["created_at"] = $timestamp;
+
             $timeClaim = $row["claimed_at"];
             $timestampClaim = strtotime( $timeClaim );
             $donation["claimed_at"] = $timestampClaim;
+
+            $timeDelivered = $row["delivered_at"];
+            $timestampDelivered = strtotime( $timeDelivered );
+            $donation["delivered_time"]=$timeDelivered;
+            $donation["delivered_at"] = $timestampDelivered;
+
             $donation["claim_code"] = $row["claim_code"];
             $donation["category"] = $row["category"];
             $donation["subCategory"] = $row["subCategory"];
@@ -55,7 +84,7 @@ if (!(empty($_POST['email']))) {
 
 
             // push single product into final response array
-            array_push($response["claimedDonations"], $donation);
+            array_push($response["SelectedDonationsFeed"], $donation);
         }
         // success
         $response["success"] = 1;
@@ -65,7 +94,10 @@ if (!(empty($_POST['email']))) {
     } else {
         // no products found
         $response["success"] = 0;
-        $response["message"] = "You have not claimed any donations yet!";
+        if($tag == "MyClaimedDonations")
+            $response["message"] = "You have no claimed donations yet!";
+        else
+            $response["message"] = "You have no delivered donations yet!";
 
         // echo no users JSON
         echo json_encode($response);
@@ -80,3 +112,4 @@ if (!(empty($_POST['email']))) {
 }
 
 ?>
+	
